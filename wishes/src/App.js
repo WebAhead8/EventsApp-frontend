8a7a9df101280d5f08d8de35c534407635fb6710
Hadi Moallem <hadimoallem1999@gmail.com>
2021-03-28 19:14:10 +0300

    addEventAndAddWishes

--- wishes/src/App.js
+++ wishes/src/App.js
@@ -4,20 +4,25 @@ import Login from './components/Login'
 import Events from './components/Events'
 import SingleEvent from './components/SingleEvent'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import AddEvent from './components/AddEvent';
+import AddWish from './components/AddWish';
 
 function App() {
   return (
     <Router>
-    <div className='App'>
+      <div className='App'>
 
-      <Switch>
-        <Route exact path='/' component={Login}></Route>
-        <Route exact path='/signup' component={SignUp}></Route>
-        <Route exact path='/events' component={Events}></Route>
-        <Route exact path='/events/:eventId' component={SingleEvent} ></Route>
-      </Switch>
-    </div>
-  </Router>
+        <Switch>
+          <Route exact path='/' component={Login}></Route>
+          <Route exact path='/signup' component={SignUp}></Route>
+          <Route exact path='/events' component={Events}></Route>
+          <Route exact path='/events/:eventId' component={SingleEvent} ></Route>
+          <Route exact path='/addEvent' component={AddEvent} ></Route>
+          <Route exact path='/addWish' component={AddWish} ></Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
