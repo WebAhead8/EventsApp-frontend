07be1fd1beae99af2761299f621658d5c8761dd1
Ebraheem <ave.brhom@gmail.com>
2021-03-30 14:32:41 +0300

    edit user details

--- wishes/src/components/UserCard.js
+++ wishes/src/components/UserCard.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import "../Style/UserCard.css";
 import { getUserByToken } from "../Fetches/getUserByToken"
-
+import EditIcon from '@material-ui/icons/Edit';
+import SaveIcon from '@material-ui/icons/Save';
+import {editUser} from "../Fetches/editUserDetails";
 
 function UserCard() {
+  const [editing, setEditing]=React.useState(false);
   const [firstName, setFirstName] = React.useState("");
   const [lastName, setLastName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [phone, setPhone] = React.useState("");
   const [birthday, setBirthday] = React.useState();
-
+const [profileImage,setProfileImage]=React.useState("https://lh3.googleusercontent.com/proxy/zg9ckdTiNj5VhgGpKiWFtNJYMHQjFqjUz8P_Mk8q_tS2aDQDTPAh37Hc4-XY75mWB2NrfzMGNrpXYdVifpBAfqQiBDwnUaA")
   const history = useHistory();
 
 
@@ -29,6 +32,10 @@ function UserCard() {
             setEmail(data[0].email)
             setPhone(data[0].phoneNumber)
             setBirthday(data[0].birthday)
+            if(data[0].profileImage)
+            {
+              setProfileImage(data[0].profileImage);
+            }
           }
         })
         .catch((error) => {
@@ -38,7 +45,53 @@ function UserCard() {
   }, []);
 
 
+if (editing)
+{
+  return (
+    <div className="userCardMainDiv">
 
+      <div className="information">
+        <input value={firstName} onChange={e=>{setFirstName(e.target.value)}}/>
+        <input value={lastName} onChange={e=>{setLastName(e.target.value)}}/>
+        <label>{email}</label>
+        <input value={phone} onChange={e=>{setPhone(e.target.value)}}/>
+        <input value={birthday} onChange={e=>{setBirthday(e.target.value)}}/>
+      <span onClick={e=>{
+        const obj={firstName:firstName,
+          lastName:lastName,
+          phoneNumber:phone,
+          email:email,
+          birthday:birthday,
+          profileImage:profileImage
+          }
+          editUser(localStorage.getItem("user"),obj).then(res=>res.json())
+          .then(data=>{
+console.log(data)
+          })
+        
+        console.log({firstName:firstName,
+        lastName:lastName,
+        phoneNumber:phone,
+        email:email,
+        birthday:birthday
+        })
+
+        
+        setEditing(false)
+      }}><SaveIcon/></span>
+      </div>
+
+
+      <div className="imageDiv">
+        <img src={profileImage}></img>
+        <input value={profileImage} onChange={e=>{setProfileImage(e.target.value)}}/>
+
+      </div>
+     
+    </div>
+  );
+
+}
   return (
     <div className="userCardMainDiv">
 
@@ -47,13 +100,17 @@ function UserCard() {
         <label>{email}</label>
         <label>{phone}</label>
         <label>{birthday}</label>
+      <span onClick={e=>{
+setEditing(true)
 
+      }}><EditIcon/></span>
       </div>
 
 
       <div className="imageDiv">
-        <img src="https://lh3.googleusercontent.com/proxy/zg9ckdTiNj5VhgGpKiWFtNJYMHQjFqjUz8P_Mk8q_tS2aDQDTPAh37Hc4-XY75mWB2NrfzMGNrpXYdVifpBAfqQiBDwnUaA"></img>
+        <img src={profileImage}></img>
       </div>
+     
     </div>
   );
 
